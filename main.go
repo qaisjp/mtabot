798f367764fdffbd38c535cb5587ece6361e34c4
Qais Patankar <qaisjp@gmail.com>
2020-01-02 02:46:10 +0000

    send feed if filename endswith exe

--- main.go
+++ main.go
@@ -26,6 +26,8 @@ var modRoles = []string{
 }
 
 const modLogChannel = "303958138489667584"
+const feedChannel = "486298246138953749"
+const privateChannelGroup = "360863453348626452"
 
 const pChatInfoSeparator = "DO NOT MODIFY FROM THIS POINT ONWARDS:"
 const pChatInstructions = "- Use `!pchat start` to invite the user to this channel." + `
@@ -102,6 +104,57 @@ var mee6inform = map[string]int{
 	"!kick":    2,
 }
 
+func (b *bot) checkMessageAttachments(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var heuristics []string
+	filenameValue := ""
+	for _, a := range m.Attachments {
+		if strings.HasSuffix(a.Filename, ".exe") {
+			heuristics = append(heuristics, "has suffix `.exe`")
+		}
+
+		if filenameValue != "" {
+			filenameValue += ", "
+		}
+		filenameValue += a.Filename
+	}
+
+	heuristicText := "Heuristic"
+	if length := len(heuristics); length == 0 {
+		return
+	} else if length > 1 {
+		heuristicText += "s"
+	}
+
+	filenameText := "Filename"
+	if len(m.Attachments) > 1 {
+		filenameText += "s"
+	}
+
+	_, _ = s.ChannelMessageSendEmbed(feedChannel, &discordgo.MessageEmbed{
+		URL: composeMessageURL(m.Message),
+		Author: &discordgo.MessageEmbedAuthor{
+			Name:    m.Author.String(),
+			IconURL: m.Author.AvatarURL(""),
+		},
+		Timestamp:   string(m.Timestamp),
+		Color:       0xffa500,
+		Title:       "A potentially malicious message has been sent.",
+		Description: heuristicText + ": " + strings.Join(heuristics, ", "),
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   filenameText,
+				Value:  filenameValue,
+				Inline: true,
+			},
+			{
+				Name:   "Channel",
+				Value:  "<#" + m.ChannelID + ">",
+				Inline: true,
+			},
+		},
+	})
+}
+
 func (b *bot) onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Limit to MTA guild only
 	if m.GuildID != guild {
@@ -114,6 +167,16 @@ func (b *bot) onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) 
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
+	}
+
+	channel, err := s.Channel(m.ChannelID)
+	if err != nil {
+		fmt.Println("[error] unknown channel", m.ChannelID)
+		return
+	}
+
+	if channel.ParentID != privateChannelGroup {
+		b.checkMessageAttachments(s, m)
 	}
 
 	if karmaRegexp.MatchString(m.Content) {

3756dfb811739710ce721be09fefc2d9b9fef915
Jordan Banasik <woovie@woovie.net>
2020-02-06 13:45:37 -0600

    Add message removal and alert that a moderator has been notified (#6)

--- modrequest.go
+++ modrequest.go
@@ -8,6 +8,7 @@ import (
 )
 
 const modRequest = "<@%s> requested a moderator in <#%s> here: %s\nReason: %s"
+const requestRecv = "Your request was received <@%s>, a moderator will review and report back soon."
 
 func (b *bot) requestMod(m *discordgo.Message, parts []string) {
 	url := composeMessageURL(m)
@@ -17,4 +18,6 @@ func (b *bot) requestMod(m *discordgo.Message, parts []string) {
 		message = strings.Join(parts, " ")
 	}
 	b.discord.ChannelMessageSend(feedChannel, fmt.Sprintf(modRequest, m.Author.ID, m.ChannelID, url, message))
+	b.sendQuickPM(m.Author.ID, fmt.Sprintf(requestRecv, m.Author.ID))
+	b.discord.ChannelMessageDelete(m.ChannelID, m.ID)
 }

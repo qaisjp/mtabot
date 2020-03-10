f2e8fa267d91c1c8b8e3866cacdf02439c7d8e4b
Qais Patankar <qaisjp@gmail.com>
2020-03-10 21:57:22 +0000

    topic: don't bold too much

--- topic.go
+++ topic.go
@@ -19,7 +19,7 @@ func (b *Bot) cmdTopic(_ string, _ *discordgo.Session, m *discordgo.Message, _ [
 		log.Printf("!topic error - could not delete message %s in channel %s in guild %s\n", m.ID, m.ChannelID, m.GuildID)
 	}
 
-	_, err = b.discord.ChannelMessageSend(m.ChannelID, fmt.Sprintf("**Topic (requested by <@%s>)**\n%s", m.Author.ID, c.Topic))
+	_, err = b.discord.ChannelMessageSend(m.ChannelID, fmt.Sprintf("**Topic** (requested by <@%s>)\n%s", m.Author.ID, c.Topic))
 	if err != nil {
 		log.Println("!topic error - could not send to channel " + m.ChannelID + " in guild " + m.GuildID)
 		return
